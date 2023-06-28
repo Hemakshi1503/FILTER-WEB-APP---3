3131abd7e3e43c9f78b5f0ee1009d8ea54297756
Hemakshi1503 <98659251+Hemakshi1503@users.noreply.github.com>
2023-06-28 14:16:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+    lips = loadImage('https://i.postimg.cc/PxFvYgkv/l1.png')
+}
+
+function setup() {
+    canvas = createCanvas(300, 300); 
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded() {
+   console.log('PoseNet Is Intitialised');
+}
+
+function gotPoses(results) 
+{
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(lips, noseX-20, noseY+20, 40, 30);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
